73ca3d22ac6e7e8815f8fee76c5d57b1e6f25463
Andrew2432 <drew.devops@gmail.com>
2020-05-16 19:07:15 +0530

    Develop Header

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Header from '../Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
 
