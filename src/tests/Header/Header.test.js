cd27627f7955139bf7f2cd260c34a4a2078ea7a1
Andrew2432 <drew.devops@gmail.com>
2020-05-16 20:10:52 +0530

    Improve test architecture

--- src/tests/Header/Header.test.js
+++ src/tests/Header/Header.test.js
@@ -2,16 +2,26 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Header from '../../components/Header/Header';
 
+const setup = (props) => {
+  const component = shallow(<Header {...props} />);
+  return component;
+};
+
 describe('Header Component', () => {
+  let component;
+
+  // Runs before each test
+  beforeEach(() => {
+    component = setup();
+  });
+
   it('Should render without errors', () => {
-    const component = shallow(<Header />);
     // console.log(component.debug());
     const navWrapper = component.find('nav.container');
     expect(navWrapper.length).toBe(1);
   });
 
   it('Should have one logo div', () => {
-    const component = shallow(<Header />);
     const logoWrapper = component.find('div.logo');
     expect(logoWrapper.length).toBe(1);
   });
