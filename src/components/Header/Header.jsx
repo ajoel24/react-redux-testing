1b187af3ca55a0541ca934e8c6421c88e3a6154c
Andrew2432 <drew.devops@gmail.com>
2020-05-16 19:49:31 +0530

    Implement Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import './Header.scss';
 
 const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header>
+      <nav className="container">
+        <div className="logo">Testing!</div>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
