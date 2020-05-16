db42aed6189b3e12b96a974752e6d766bf40780d
Andrew2432 <drew.devops@gmail.com>
2020-05-16 20:06:39 +0530

    Write tests for Header

--- src/tests/Header/Header.test.js
+++ src/tests/Header/Header.test.js
@@ -2,6 +2,17 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Header from '../../components/Header/Header';
 
-it('should render without errors', () => {
-  expect(1).toBe(1);
+describe('Header Component', () => {
+  it('Should render without errors', () => {
+    const component = shallow(<Header />);
+    // console.log(component.debug());
+    const navWrapper = component.find('nav.container');
+    expect(navWrapper.length).toBe(1);
+  });
+
+  it('Should have one logo div', () => {
+    const component = shallow(<Header />);
+    const logoWrapper = component.find('div.logo');
+    expect(logoWrapper.length).toBe(1);
+  });
 });
