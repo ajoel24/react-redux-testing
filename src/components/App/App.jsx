252a2d0d3839b4e7c1961b2f11d621a65083519f
Andrew2432 <drew.devops@gmail.com>
2020-05-16 20:39:49 +0530

    Add Headline to App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import Header from '../Header/Header';
+import Headline from '../Headline/Headline';
 import './App.scss';
 
 function App() {
   return (
-    <>
+    <Fragment>
       <Header />
-      <main></main>
-    </>
+      <main>
+        <Headline title="Post" desc="Click the button to render posts" />
+      </main>
+    </Fragment>
   );
 }
 
