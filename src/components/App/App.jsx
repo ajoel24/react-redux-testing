749f826f6a678dadcd7f13d16bb49b8cf29e4803
Andrew2432 <drew.devops@gmail.com>
2020-05-17 14:09:59 +0530

    Implement PropTypes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,13 @@
 import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../Header/Header';
 import Headline from '../Headline/Headline';
 import './App.scss';
+
+Headline.propTypes = {
+  title: PropTypes.string,
+  desc: PropTypes.string,
+};
 
 function App() {
   return (
